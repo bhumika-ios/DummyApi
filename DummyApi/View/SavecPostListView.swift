b01d4bdd82f87167b9514bb72ada9d70d858b08e
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-01 12:37:27 +0530

    create file of sace postlistview

---
+++ DummyApi/View/SavecPostListView.swift
@@ -0,0 +1,18 @@
+//
+//  SavecPostListView.swift
+//  DummyApi
+//
+//  Created by Bhumika Patel on 01/03/24.
+//
+
+import SwiftUI
+
+struct SavecPostListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavecPostListView()
+}
