8ceb51c04d50da17ec4f8d963d3620cd953e789a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-01 12:28:05 +0530

    define array value from api then call in model

--- DummyApi/Model/PostModel.swift
+++ DummyApi/Model/PostModel.swift
@@ -14,4 +14,30 @@ struct Post: Codable, Identifiable {
     let tags: [String]
     let text: String
     let publishDate: String
+    let owner: Owner
+ //   var isSaved: Bool = false
+
+    var formattedDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        if let date = dateFormatter.date(from: publishDate) {
+            dateFormatter.dateFormat = "E, hh:mm a"
+            return dateFormatter.string(from: date)
+        }
+        return publishDate
+    }
+}
+struct Owner: Codable {
+    let id: String
+    let title: String
+    let firstName: String
+    let lastName: String
+    let picture: String
+}
+
+struct PostResponse1: Codable {
+    let data: [Post]
+    let total: Int
+    let page: Int
+    let limit: Int
 }
