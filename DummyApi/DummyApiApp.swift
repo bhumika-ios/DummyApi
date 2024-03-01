9714ab0c9c878dd1a7ee9fdb292cccc405cf7a6a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-01 12:10:32 +0530

    Initial Commit

---
+++ DummyApi/DummyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyApiApp.swift
+//  DummyApi
+//
+//  Created by Bhumika Patel on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DummyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
