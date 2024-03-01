6551eef236139f0d651a65fb2b780d90564b523d
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-01 12:32:36 +0530

    set of background color and call post value for fetch value

--- DummyApi/View/PostDetailsView.swift
+++ DummyApi/View/PostDetailsView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct PostDetailsView: View {
+    let post: Post
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.gray.opacity(0.2)
+                .ignoresSafeArea()
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    // Image
+                    UrlImage(url: post.image)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 360,height: 250)
+                        .clipped()
+                    
+                    // Post details
+                    VStack(alignment: .leading){
+                        HStack{
+                            Text(post.text)
+                                .font(.system(size: 24))
+                                .bold()
+                                
+                            Spacer()
+                        }
+                        HStack {
+                            Text("\(post.likes) likes")
+                            Spacer()
+                            // Text("Comments: \(/* Implement comments count */post.)")
+                        }
+                        Divider()
+                        Text(post.text)
+                            .font(.title3)
+                        Spacer()
+                        Text("Posted on: \(post.publishDate)")
+                        
+                        Text("Owner: \(post.owner.firstName) \(post.owner.lastName)")
+                        
+                         
+                        
+                        // Additional details can be added here
+                        
+                    }
+                   // .padding(.horizontal)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+            }
+        }
+                          
     }
 }
 
-#Preview {
-    PostDetailsView()
-}
+//#Preview {
+//    PostDetailsView()
+//}
