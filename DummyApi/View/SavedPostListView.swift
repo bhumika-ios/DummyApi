bd44a4fec4405d89bae94f0e6412d9e11b84b7d1
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-01 12:40:01 +0530

    binfing value of post and saveposted for filter used

--- DummyApi/View/SavedPostListView.swift
+++ DummyApi/View/SavedPostListView.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct SavedPostListView: View {
+    @Binding var savedPosts: [Post]
+       @ObservedObject var viewModel: PostViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        ForEach(savedPosts.filter { viewModel.isPostSaved(post: $0) }) { post in
+                            
+                        }
+                    }
+                    .padding()
+                }
     }
 }
 
-#Preview {
-    SavedPostListView()
-}
+//#Preview {
+//    SavedPostListView()
+//}
