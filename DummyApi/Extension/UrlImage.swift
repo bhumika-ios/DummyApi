8773b820260abc1858db35e9e19a9e077e8255c6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-01 12:21:08 +0530

    create file urlimage and call for fetch of image

---
+++ DummyApi/Extension/UrlImage.swift
@@ -0,0 +1,18 @@
+//
+//  UrlImage.swift
+//  DummyApi
+//
+//  Created by Bhumika Patel on 01/03/24.
+//
+
+import SwiftUI
+
+struct UrlImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UrlImage()
+}
